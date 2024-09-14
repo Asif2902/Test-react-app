ad74234eebda26f6bbacae065229e00b115fc7dc
Asif <107271220+Asif2902@users.noreply.github.com>
2024-09-15 04:07:01 +0600

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Onboard } from '@web3-onboard/react';
+import { chains } from './chains';
 import './styles.css';
+
+const onboard = Onboard({
+  chains,
+  wallets: [
+    { name: 'MetaMask', wallets: ['metamask'] },
+    { name: 'WalletConnect', wallets: ['walletConnect'] },
+  ],
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Onboard>
+      <App />
+    </Onboard>
   </React.StrictMode>,
   document.getElementById('root')
 );
