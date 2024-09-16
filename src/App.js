de81a8e5d6dfcebb95a16a830df8983e1ac723ff
Asif <107271220+Asif2902@users.noreply.github.com>
2024-09-16 21:11:58 +0600

    update src/App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import StakeSection from './components/StakeSection';
 import UnstakeSection from './components/UnstakeSection';
 import WithdrawSection from './components/WithdrawSection';
 
-
 const stakingABI = [ { "inputs": [ { "internalType": "contract IERC20", "name": "_rewardToken", "type": "address" }, { "internalType": "address", "name": "initialOwner", "type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" } ], "name": "OwnershipTransferred", "type": "event" }, { "inputs": [], "name": "APY", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "SECONDS_IN_A_YEAR", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "_staker", "type": "address" } ], "name": "getReward", "outputs": [ { "internalType": "address", "name": "", "type": "address" }, { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getTotalStaked", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "rewardToken", "outputs": [ { "internalType": "contract IERC20", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "stake", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "stakers", "outputs": [ { "internalType": "uint256", "name": "amountStaked", "type": "uint256" }, { "internalType": "uint256", "name": "lastStakedTime", "type": "uint256" }, { "internalType": "uint256", "name": "rewardDebt", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalStaked", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "newOwner", "type": "address" } ], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "_amount", "type": "uint256" } ], "name": "unstake", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "withdrawReward", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]; 
-
+ // Use your staking contract ABI
 const stakingContractAddress = '0xcE3E021038C4f62209EFf23f1d2D3B3EbE83b600';
 
 function App() {
@@ -19,16 +18,14 @@ function App() {
   const [walletBalance, setWalletBalance] = useState('0');
   const [rewardAvailable, setRewardAvailable] = useState('0');
 
-  const minatoNetwork = {
-    chainId: '0x79a',
-    chainName: 'Minato',
-    nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
-    rpcUrls: ['https://rpc.minato.soneium.org'],
-    blockExplorerUrls: ['https://explorer-testnet.soneium.org']
-  };
+  useEffect(() => {
+    if (signer) {
+      const stakingContract = new ethers.Contract(stakingContractAddress, stakingABI, signer);
+      setContract(stakingContract);
+    }
+  }, [signer]);
 
-  // Fetch total staked and ETH price from CoinGecko
-  async function updateTotalStaked() {
+  const updateTotalStaked = async () => {
     if (contract) {
       const totalStakedValue = await contract.getTotalStaked();
       const ethPrice = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
@@ -36,23 +33,21 @@ function App() {
       setTotalStaked(totalStakedETH);
       setEthInUSD((totalStakedETH * ethPrice.data.ethereum.usd).toFixed(2));
     }
-  }
+  };
 
-  // Fetch Wallet Balance
-  async function updateWalletBalance() {
+  const updateWalletBalance = async () => {
     if (provider && walletAddress) {
       const balance = await provider.getBalance(walletAddress);
       setWalletBalance(ethers.utils.formatEther(balance));
     }
-  }
+  };
 
-  // Fetch Rewards
-  async function updateRewards() {
+  const updateRewards = async () => {
     if (contract && walletAddress) {
       const [_, reward] = await contract.getReward(walletAddress);
       setRewardAvailable(ethers.utils.formatUnits(reward, 18));
     }
-  }
+  };
 
   useEffect(() => {
     if (contract) {
